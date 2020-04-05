e295c2cf2771de290ce479361657f53ac012d057
Jacklu <jacklu.net@gmail.com>
2020-04-05 15:54:46 +0000

    增加 CSRF 验证

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -17,11 +17,27 @@ class UsersController extends Controller
     }
 
     /**
+     * 获取用户
      * @param User $user
      * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
      */
     public function show(User $user)
     {
         return view('users.show', compact('user'));
+    }
+
+    /**
+     * 用户注册
+     * @param Request $request
+     * @throws \Illuminate\Validation\ValidationException
+     */
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'name' => 'required|unique:users|max:50',
+            'email' => 'required|email|unique:users|max:255',
+            'password' => 'required|confirmed|min:6'
+        ]);
+        return;
     }
 }
