76bfd48b152b5a5ce990d12da3a5a5d4972d0017
Jacklu <jacklu.net@gmail.com>
2020-04-05 17:51:20 +0000

    用户退出登录

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -42,4 +42,15 @@ class SessionsController extends Controller
             return redirect()->back()->withInput();
         }
     }
+
+    /**
+     * 退出
+     * @return \Illuminate\Http\RedirectResponse|\Illuminate\Routing\Redirector
+     */
+    public function destroy()
+    {
+        Auth::logout();
+        session()->flash('success', '你已成功退出！');
+        return redirect('login');
+    }
 }
