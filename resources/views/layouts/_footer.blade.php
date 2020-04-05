df00bafa85fa2c9bd7c2d6211f0def511577285e
Jacklu <jacklu.net@gmail.com>
2020-04-05 15:19:14 +0800

    切割头部和尾部子视图

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,8 @@
+<footer class="footer">
+    <img class="brand-icon" src="{{ 'https://www.gravatar.com/avatar/' . md5(strtolower(trim('jacklu.net@gmail.com'))) . '?s=50' }}" alt="">
+    <a href="https://jacklucn.com" target="_blank">Jacklu</a>
+
+    <div class="float-right">
+        <a href="/about">关于</a>
+    </div>
+</footer>
