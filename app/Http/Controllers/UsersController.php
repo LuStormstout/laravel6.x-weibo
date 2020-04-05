0a3aec2ac7c878819427f2f71b8c67d9d9a0daf5
Jacklu <jacklu.net@gmail.com>
2020-04-05 15:39:16 +0000

    用户显示页面

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class UsersController extends Controller
@@ -13,5 +14,14 @@ class UsersController extends Controller
     public function create()
     {
         return view('users.create');
+    }
+
+    /**
+     * @param User $user
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
+    public function show(User $user)
+    {
+        return view('users.show', compact('user'));
     }
 }
