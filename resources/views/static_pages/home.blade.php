7c1027a85d06e4b87d32095ac804d30c83ba7563
Jacklu <jacklu.net@gmail.com>
2020-04-05 06:40:46 +0000

    初始化样式

--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -1,4 +1,16 @@
 @extends('layouts.default')
+
 @section('content')
-    <h1>主页</h1>
+    <div class="jumbotron">
+        <h1>Hello</h1>
+        <p class="lead">
+            你现在看到的是基于 Laravel 打造的 「weibo」 练习项目。
+        </p>
+        <p>
+            一切，将从这里开始。
+        </p>
+        <p>
+            <a class="btn btn-lg btn-success" href="#" role="button">现在注册</a>
+        </p>
+    </div>
 @stop
