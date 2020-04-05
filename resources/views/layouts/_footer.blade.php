c73435741a541a25fc43668c82edbab629d3aa1a
Jacklu <jacklu.net@gmail.com>
2020-04-05 15:24:46 +0800

    命名路由

--- resources/views/layouts/_footer.blade.php
+++ resources/views/layouts/_footer.blade.php
@@ -1,8 +1,10 @@
 <footer class="footer">
-    <img class="brand-icon" src="{{ 'https://www.gravatar.com/avatar/' . md5(strtolower(trim('jacklu.net@gmail.com'))) . '?s=50' }}" alt="">
+    <img class="brand-icon"
+         src="{{ 'https://www.gravatar.com/avatar/' . md5(strtolower(trim('jacklu.net@gmail.com'))) . '?s=50' }}"
+         alt="">
     <a href="https://jacklucn.com" target="_blank">Jacklu</a>
 
     <div class="float-right">
-        <a href="/about">关于</a>
+        <a href="{{ route('about') }}">关于</a>
     </div>
 </footer>
