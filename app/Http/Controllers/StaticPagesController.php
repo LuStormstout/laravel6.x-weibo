6ea58e93f5855323434199d332bf7a3e22ee2247
Jacklu <jacklu.net@gmail.com>
2020-04-05 15:46:01 +0800

    用户注册页面

--- app/Http/Controllers/StaticPagesController.php
+++ app/Http/Controllers/StaticPagesController.php
@@ -6,16 +6,28 @@ use Illuminate\Http\Request;
 
 class StaticPagesController extends Controller
 {
+    /**
+     * 首页
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
     public function home()
     {
         return view('static_pages/home');
     }
 
+    /**
+     * 帮助
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
     public function help()
     {
         return view('static_pages/help');
     }
 
+    /**
+     * 关于
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
     public function about()
     {
         return view('static_pages/about');
