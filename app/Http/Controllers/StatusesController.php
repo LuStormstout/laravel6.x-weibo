f8daea5651409a5b3f35fec148a8fba470fe592a
Jacklu <jacklu.net@gmail.com>
2020-05-06 08:45:12 +0000

    删除微博

--- app/Http/Controllers/StatusesController.php
+++ app/Http/Controllers/StatusesController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Status;
+use Illuminate\Auth\Access\AuthorizationException as AuthorizationExceptionAlias;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
@@ -33,6 +35,20 @@ class StatusesController extends Controller
             'content' => $request['content']
         ]);
         session()->flash('success', '发布成功！');
+        return redirect()->back();
+    }
+
+    /**
+     * 删除微博
+     * @param Status $status
+     * @return RedirectResponse
+     * @throws AuthorizationExceptionAlias
+     */
+    public function destroy(Status $status)
+    {
+        $this->authorize('destroy', $status);
+        $status->delete();
+        session()->flash('success', '微博已被成功删除！');
         return redirect()->back();
     }
 }
---
+++ app/Policies/StatusPolicy.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\Status;
+use App\Models\User;
+use Illuminate\Auth\Access\HandlesAuthorization;
+
+class StatusPolicy
+{
+    use HandlesAuthorization;
+
+    /**
+     * 只有作者可以删除微博
+     * @param User $user
+     * @param Status $status
+     * @return bool
+     */
+    public function destroy(User $user, Status $status)
+    {
+        return $user->id === $status->user_id;
+    }
+}
